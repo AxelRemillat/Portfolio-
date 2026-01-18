2989526c42e52471444b12a7411784f23b00efb5
AxelRemillat <atlas.flaeme@gmail.com>
2026-01-18 22:23:10 +0100

    Create Vous & Moi page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import "./styles/index.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Home from "./pages/Home";
 import Parcours from "./pages/Parcours";
-import About from "./pages/About";
+import VousEtMoiPage from "./pages/vous-et-moi/VousEtMoiPage";
 import ContactPage from "./pages/Contact";
 
 export default function App() {
@@ -13,7 +13,8 @@ export default function App() {
         <Route element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/parcours" element={<Parcours />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/vous-et-moi" element={<VousEtMoiPage />} />
+          <Route path="/about" element={<Navigate to="/vous-et-moi" replace />} />
           <Route path="/contact" element={<ContactPage />} />
         </Route>
       </Routes>
